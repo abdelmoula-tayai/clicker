d8b542de6d03528ed0eddb57aa96644353f89d58
Your Name <you@example.com>
2024-04-16 08:59:23 +0200

    ajout logique pour augmenter l'argent obtenu sur le clique

--- script/script.js
+++ script/script.js
@@ -2,12 +2,16 @@ const money = document.getElementById('money');
 const click = document.getElementById('click');
 const showModal = document.getElementById('shop');
 const modal = document.getElementById('modal');
+const buyX10 = document.getElementById('buyButtonX10');
 
 click.addEventListener('click', increaseMoney);
 
+
+let moneyIncrement = 1;
+
 function increaseMoney() {
     let currentMoney = parseInt(money.innerText);
-    money.innerText = (currentMoney + 1) + ' $';
+    money.innerText = (currentMoney + moneyIncrement) + ' $';
     
 }
 
@@ -17,5 +21,17 @@ showModal.addEventListener('click', () => {
 });
 
 closeModal.addEventListener("click", () => {
+    modal.classList.add('hidden');
+});
+
+
+buyX10.addEventListener('click', () => {
+    let currentMoney = parseInt(money.innerText);
+    if (currentMoney >= 100) {
+        money.innerText = (currentMoney - 100) + ' $';
+        moneyIncrement = 10;
+    } else {
+        alert('Vous n\'avez pas assez d\'argent pour acheter cet article');
+    }
     modal.classList.add('hidden');
 });
