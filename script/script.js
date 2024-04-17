51a02805cef0676703dffcadb5748919aa8cd4b4
abdelmoula-tayai <145970067+abdelmoula-tayai@users.noreply.github.com>
2024-04-17 08:16:44 +0200

    derniere version fonctionnel

--- script/script.js
+++ script/script.js
@@ -8,12 +8,22 @@ const click = document.getElementById('click');
 const showModal = document.getElementById('shop');
 const modal = document.getElementById('modal');
 const buyX10 = document.getElementById('buyButtonX10');
+const buyX10disabled = document.getElementById('buyButtonX10disabled');
 const buyX20 = document.getElementById('buyButtonX20');
+const buyX20disabled = document.getElementById('buyButtonX20disabled');
 const buyX50 = document.getElementById('buyButtonX50');
+const buyX50disabled = document.getElementById('buyButtonX50disabled');
 const buyX100 = document.getElementById('buyButtonX100');
+const buyX100disabled = document.getElementById('buyButtonX100disabled');
 
 // Charger moneyIncrement depuis le stockage local ou utiliser la valeur par défaut (1)
 let moneyIncrement = parseInt(localStorage.getItem('moneyIncrement')) || 1;
+let hiddenButtons = JSON.parse(localStorage.getItem('hiddenButtons')) || [];
+let shownButtons = JSON.parse(localStorage.getItem('shownButtons')) || [];
+
+// Si des identifiants sont enregistrés, masquer et afficher les boutons correspondants
+hiddenButtons.forEach(buttonId => document.getElementById(buttonId).classList.add('hidden'));
+shownButtons.forEach(buttonId => document.getElementById(buttonId).classList.remove('hidden'));
 
 // Fonction pour incrémenter l'argent
 function increaseMoney() {
@@ -40,20 +50,28 @@ closeModal.addEventListener("click", () => {
 });
 
 // Fonction pour acheter un bonus x10, x20, x50 ou x100
-function buyBonus(incrementValue, cost) {
+function buyBonus(incrementValue, cost, buttonToHide, buttonToShow) {
     let currentMoney = parseInt(money.innerText);
     if (currentMoney >= cost) {
-        money.innerText = (currentMoney - cost) + ' $'; // Soustraire le coût de l'article acheté
-        moneyIncrement = incrementValue; // Mettre à jour le montant de l'incrémentation de l'argent
-        localStorage.setItem('moneyIncrement', moneyIncrement); // Sauvegarde de la nouvelle valeur de l'incrémentation dans le stockage local
+        money.innerText = (currentMoney - cost) + ' $';
+        moneyIncrement = incrementValue;
+        localStorage.setItem('moneyIncrement', moneyIncrement);
+        buttonToHide.classList.add('hidden');
+        buttonToShow.classList.remove('hidden');
+        // Ajouter les IDs des boutons masqués et affichés aux tableaux
+        hiddenButtons.push(buttonToHide.id);
+        shownButtons.push(buttonToShow.id);
+        // Enregistrer les tableaux d'IDs de boutons dans le stockage local
+        localStorage.setItem('hiddenButtons', JSON.stringify(hiddenButtons));
+        localStorage.setItem('shownButtons', JSON.stringify(shownButtons));
     } else {
         alert('Vous n\'avez pas assez d\'argent pour acheter cet article');
     }
-    modal.classList.add('hidden'); // Masquer le modal après l'achat
+    modal.classList.add('hidden');
 }
 
-// Écouteurs d'événement pour acheter différents bonus
-buyX10.addEventListener('click', () => buyBonus(10, 100)); // Achat du bonus x10
-buyX20.addEventListener('click', () => buyBonus(20, 500)); // Achat du bonus x20
-buyX50.addEventListener('click', () => buyBonus(50, 1500)); // Achat du bonus x50
-buyX100.addEventListener('click', () => buyBonus(100, 3500)); // Achat du bonus x100
+
+buyX10.addEventListener('click', () => buyBonus(10, 100, buyX10, buyX10disabled)); // appelle la fonction buyBonus avec les paramètres correspondants
+buyX20.addEventListener('click', () => buyBonus(20, 500, buyX20, buyX20disabled)); // appelle la fonction buyBonus avec les paramètres correspondants
+buyX50.addEventListener('click', () => buyBonus(50, 1500, buyX50, buyX50disabled)); // appelle la fonction buyBonus avec les paramètres correspondants
+buyX100.addEventListener('click', () => buyBonus(100, 3500, buyX100, buyX100disabled)); // appelle la fonction buyBonus avec les paramètres correspondants
